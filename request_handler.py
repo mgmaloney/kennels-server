ee0cc889cb4fbe237c5b1d4d53f0473e3e7e93f9
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-10-16 21:08:33 -0500

    post requests complete

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, get_single_employee, get_all_employees, get_all_customers, get_single_customer
+from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, get_single_employee, get_all_employees, get_all_customers, get_single_customer, create_animal, create_customer, create_employee, create_location, delete_animal
 
 
 # Here's a class. It inherits from another class.
@@ -101,15 +101,39 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+        new_customer = None
+        new_employee = None
+        new_location = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+            self.wfile.write(json.dumps(new_animal).encode())
+            
+        if resource == 'locations':
+            new_location = create_location(post_body)
+            self.wfile.write(json.dumps(new_location).encode())
+        if resource == 'customers':
+            new_customer = create_customer(post_body)
+            self.wfile.write(json.dumps(new_customer).encode())
+        if resource == 'employees':
+            new_employee = create_employee(post_body)
+            self.wfile.write(json.dumps(new_employee).encode())
+            # Encode the new animal and send in response 
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
@@ -118,6 +142,20 @@ class HandleRequests(BaseHTTPRequestHandler):
         """Handles PUT requests to the server
         """
         self.do_POST()
+        
+    def do_DELETE(self):
+    # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
