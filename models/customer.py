ddb5e7a9972bf3d2f688999b11498b0b6757be05
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-10-22 12:45:35 -0500

    get all and single refactored for all entities

--- models/customer.py
+++ models/customer.py
@@ -1,4 +1,7 @@
 class Customer:
-    def __init__(self, name, id):
+    def __init__(self, id, name, address, email, password):
         self.name = name
         self.id = id
+        self.address = address
+        self.email = email
+        self.password = password
