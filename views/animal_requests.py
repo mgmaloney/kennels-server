b8bdfce19a3dbe4ee38c783c3f9b9848d6c43c1a
matthewmaloneydev@gmail.com <matthew@Ruobings-MacBook-Pro.local>
2023-10-23 13:44:28 -0500

    get_animals_by_status working

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -122,8 +122,32 @@ def get_animals_by_location(location_id):
         
         return animals
 
-
-
+def get_animals_by_status(status):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.name,
+            c.breed,
+            c.status,
+            c.location_id,
+            c.customer_id
+        FROM Animal c
+        WHERE status = ?                 
+        """, (status, ))
+        
+        animals = []
+        
+        dataset = db_cursor.fetchall()
+        
+        for row in dataset:
+            animal = Animal(row["id"], row["name"], row["breed"], row["status"], row["location_id"], row["customer_id"])
+            animals.append(animal.__dict__)
+        
+        return animals
 
 
 
